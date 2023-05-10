e3682885c102feef4d2f81f1b8098b9377b486ae
Paul Hayes <fofr@users.noreply.github.com>
2023-05-10 12:53:42 +0100

    Append punctuation to bark prompt
    Bark works best if there's punctuation at the end

--- pages/api/bark/index.js
+++ pages/api/bark/index.js
@@ -2,12 +2,19 @@ import createApiHandler from "../api-handler";
 
 const handler = createApiHandler(
   "b76242b40d67c76ab6742e987628a2a9ac019e11d56ab96c4e91ce03b79b2787",
-  (body) => ({
-    prompt: body.prompt,
-    history_prompt: "announcer",
-    text_temp: 0.1,
-    waveform_temp: 0.2
-  })
+  (body) => {
+    // Check if the last character is not '.', '?' or '!'
+    if (!/[.?!]$/.test(body.prompt)) {
+      body.prompt = `${body.prompt}.`;
+    }
+
+    return {
+      prompt: body.prompt,
+      history_prompt: "announcer",
+      text_temp: 0.1,
+      waveform_temp: 0.2,
+    };
+  }
 );
 
 export default handler;
