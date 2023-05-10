c9c1159c8f695393a8390fa47b8d8a007e6d018d
Paul Hayes <fofr@users.noreply.github.com>
2023-05-10 17:13:28 +0100

    Try pose for more varied results

--- pages/api/controlnet/index.js
+++ pages/api/controlnet/index.js
@@ -5,8 +5,8 @@ const handler = createApiHandler(
   (body) => ({
     prompt: body.prompt,
     n_prompt: "topless, nude, nsfw, ugly, garish, tacky, kitsch, vulgar, tasteless",
-    a_prompt: "detailed, 8k, beautiful lighting, award winning portrait, interesting background",
-    structure: "depth",
+    a_prompt: "detailed, 8k, 32k uhd, award winning, interesting",
+    structure: "pose",
     steps: 40,
     image:
       "https://cdn.discordapp.com/attachments/999037445049954324/1105606197891047584/fofr_a_portrait_photo_of_a_beautiful_man_looking_at_the_camera__a257529e-a6c4-488c-87a9-e667d7712384.png",
